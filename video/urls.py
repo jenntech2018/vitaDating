7b756395664dfb1cafa6597d1128c71542a74dad
Jalon Dodson <jalon.dodson@yahoo.com>
2021-04-06 12:31:28 -0500

    updated url config

--- video/urls.py
+++ video/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from video.views import UploadView
 
 urlpatterns = [
-    path("/upload", UploadView.as_view(), name="upload")
+    path("upload", UploadView.as_view(), name="upload")
 ]
