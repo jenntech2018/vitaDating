94d08ede5d72f042f675cb802cf612f22da932fe
Dessance Chandler <dchandler616@yahoo.com>
2021-04-06 11:11:23 -0400

    updated user model

--- vibe_user/models.py
+++ vibe_user/models.py
@@ -4,16 +4,13 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class Viber(AbstractUser):
-    username = models.CharField(max_length=120)
     display_name = models.CharField(max_length=120, null=True, blank=True)
-    bio = models.TextAreaField(blank=True)
-    followers = models.ManyToManyField('self', symmetrical=False)
-    following = models.ManyToManyField('self', symmetrical=False)
-    likes = models.IntegerField(null=True, blank=True)
-    video = models.ManyToManyField('self', symmetrical=False)
-    sound = models.ManyToManyField('self', symmetrical=False)
-    liked = models.ForeignKey(Video, on_delete=models.CASCADE)
-    profile_photo = models.ImageField(blank=True)
+    bio = models.TextField()
+    followers = models.ManyToManyField('self', symmetrical=False, related_name='viber_followers')
+    following = models.ManyToManyField('self', symmetrical=False, related_name='viber_following')
+    video = models.ManyToManyField('self', symmetrical=False, related_name='viber_video')
+    sound = models.ManyToManyField('self', symmetrical=False, related_name='viber_sound')
+    # profile_photo = models.ImageField(blank=True)
 
- def __str__(self):
+    def __str__(self):
         return self.username
