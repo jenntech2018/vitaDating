b7085645976b375c597fcbb80ba752e4984745bf
Dessance Chandler <dchandler616@yahoo.com>
2021-04-05 22:43:55 -0400

    updated user model name

--- vibe_user/models.py
+++ vibe_user/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 from django.contrib.auth.models import AbstractUser
 
-class CustomUser(AbstractUser):
+class Viber(AbstractUser):
     username = models.CharField(max_length=120)
     display_name = models.CharField(max_length=120, null=True, blank=True)
     bio = models.TextAreaField(blank=True)
