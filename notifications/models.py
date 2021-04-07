874a9ac9c1678876b7833cb3705bcd9656282273
Your Name <you@example.com>
2021-04-07 15:00:37 -0500

    completed notification model. added like function

--- notifications/models.py
+++ notifications/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.utils import timezone
+from vibe_user.models import Viber
+from video.models import Video
 # Create your models here.
 
 '''
@@ -14,5 +16,8 @@ follow
 
 class Notifications(models.model):
     time_created = models.DateTimeField(default=timezone.now)
-    mentions = models.ManyToManyField()
-    
+    mentions = models.ManyToManyField(Viber, symmetrical=False, related_name='mentions')
+    post = models.ForeignKey(Video, on_delete=models.CASCADE)
+    liked = models.ManyToManyField(Viber, symmetrical=False, related_name='liked')
+    Commented = models.ManyToManyField(Viber, symmetrical=False, related_name='commented')
+    followed = models.ManyToManyField(Viber, symmetrical=False, related_name='followed')
--- notifications/views.py
+++ notifications/views.py
@@ -1,26 +1,42 @@
 from django.shortcuts import render
 from notifications.models import Notifications
+from video.models import Video
 # Create your views here.
 '''
 def like_notification(request, post_id):
+
 
 def comment_notification(request, post_id):
 
 def follow_notifcation(request, user_id):
 
 def mention_notifcation(requst, post_id):
+    latest_post = Video.objects.latest('timestamp')
+    mentions = re.findall(r'@([A-Za-z0-9_]+)', latest_post.comments.body)
+
+
 '''
 
 
 def notification_view(request, user_id):
     user = Model.objects.get(id=user_id)
-    mention_notifications = Notifications.objects.filter(mentions=user_id).all()
-    like_notifications = Notifications.objects.filter(mentions=user_id).all()
-    comment_notifications = Notifications.objects.filter(mentions=user_id).all()
-    followed_notifications = Notifications.objects.filter(mentions=user_id).all()
+    mention_notifications = Notifications.objects.filter(id=request.user.id).all()
+    like_notifications = Notifications.objects.filter(id=request.user.id).all()
+    comment_notifications = Notifications.objects.filter(id=request.user.id).all()
+    followed_notifications = Notifications.objects.filter(id=request.user.id).all()
+
     return render(request, 'notifications.html',{
         'comments': comment_notifications,
         'likes': like_notifications,
         'mentions': mention_notifications,
         'followed': followed_notifications
     })
+
+def like_notification(request, post_id):
+    user = request.user
+    notification_object = notifications.objects.create(
+        post = post_id,
+        liked = post_id,
+
+    ).mentions.add(post_id.user)
+    notification_object.save()
