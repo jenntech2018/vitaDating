30c3f399eb72d8f7bc078f3a521d7c09ad7d2a95
Jalon Dodson <jalon.dodson@yahoo.com>
2021-04-07 11:28:45 -0500

    updated viber model, installed Pillow module for image handling

--- vibe_user/models.py
+++ vibe_user/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from video.models import Video
 # Create your models here.
 from django.contrib.auth.models import AbstractUser
 
@@ -9,9 +9,9 @@ class Viber(AbstractUser):
     dob = models.DateField()
     followers = models.ManyToManyField('self', symmetrical=False, related_name='viber_followers')
     following = models.ManyToManyField('self', symmetrical=False, related_name='viber_following')
-    video = models.ManyToManyField('self', symmetrical=False, related_name='viber_video')
-    sound = models.ManyToManyField('self', symmetrical=False, related_name='viber_sound')
-    # profile_photo = models.ImageField(blank=True)
+    videos = models.ManyToManyField(Video, related_name='viber_videos')
+    # sound = models.ManyToManyField('self', symmetrical=False, related_name='viber_sound')
+    profile_photo = models.ImageField(blank=True)
 
     def __str__(self):
         return self.username
