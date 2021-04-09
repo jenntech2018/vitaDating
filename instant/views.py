9ffe5fd8fd54d70bc4840d007cb2aae866539922
jennifer <ariesgal2017@gmail.com>
2021-04-09 16:45:25 -0700

    finished 98% messages

---
+++ instant/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render, redirect, reverse
+from django.views import View
+from .forms import MessageForm
+from .models import Chat
+
+
+class MessagesView(View):
+    def get(self, request, chat_id):
+        try:
+            chat = Chat.objects.get(id=chat_id)
+            if request.user in chat.members.all():
+                chat.message_set.filter(is_readed=False).exclude(
+                    author=request.user).update(is_readed=True)
+            else:
+                chat = None
+        except Chat.DoesNotExist:
+            chat = None
+
+        return render(
+            request,
+            'users/messages.html',
+            {
+                'user_profile': request.user,
+                'chat': chat,
+                'form': MessageForm()
+            }
+        )
+
+    def post(self, request, chat_id):
+        form = MessageForm(data=request.POST)
+        if form.is_valid():
+            message = form.save(commit=False)
+            message.chat_id = chat_id
+            message.author = request.user
+            message.save()
+        return redirect(reverse('users:messages', kwargs={'chat_id': chat_id}))
+
