4713c9129f0f9d442376e70d856dd619a0040547
Dessance Chandler <dchandler616@yahoo.com>
2021-04-09 00:38:49 -0400

    updates to user profile

--- vibe_user/views.py
+++ vibe_user/views.py
@@ -9,13 +9,13 @@ def vibe_user_profile_view(request, user_id):
     vibe_user = Viber.objects.get(id=user_id)
     vibe_followers = vibe_user.followers.all().count
     vibe_following = vibe_user.following.all().count
-    vibe_user_videos = Video.objects.get(id=user_id)
+    # vibe_user_videos = Video.objects.get(id=user_id)
     # vibe_likes = vibe_user.likes.all().count
     return render(request, "user/vibe_profile.html", {
         "vibe_user": vibe_user, 
         "vibe_followers": vibe_followers,
         "vibe_following": vibe_following, 
-        "vibe_user_videos": vibe_user_videos,
+        # "vibe_user_videos": vibe_user_videos,
         # 'vibe_likes': vibe_likes,
         })
 
