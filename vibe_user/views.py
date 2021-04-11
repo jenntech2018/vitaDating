860fdae2c420e3f325718316b488964323226a8d
Jalon Dodson <jalon.dodson@yahoo.com>
2021-04-10 20:49:42 -0500

    updated profile url path

--- vibe_user/views.py
+++ vibe_user/views.py
@@ -5,8 +5,8 @@ from vibe_user.models import Viber
 from video.models import Video
 # Create your views here.
 
-def vibe_user_profile_view(request, user_id):
-    vibe_user = Viber.objects.get(id=user_id)
+def vibe_user_profile_view(request, username):
+    vibe_user = Viber.objects.get(username=username)
     vibe_followers = vibe_user.followers.all().count
     vibe_following = vibe_user.following.all().count
     vibe_user_profile_photo = vibe_user.profile_photo
