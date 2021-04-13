7dacbd56f551cc9e2f21e28cb8f7cab06d3a05be
jennifer <ariesgal2017@gmail.com>
2021-04-13 02:07:42 -0700

    got messages link working with top navbar inbox button

--- instant/models.py
+++ instant/models.py
@@ -5,6 +5,18 @@ from django.utils.translation import ugettext_lazy as _
 from vibe_user.models import Viber
 
 
+class Message(models.Model):
+    author = models.ForeignKey(Viber, verbose_name='viber', on_delete=models.CASCADE)
+    message = models.TextField(max_length=280)
+    pub_date = models.DateTimeField(default=timezone.now)
+    is_read = models.BooleanField(default=False)
+
+    class Meta:
+        ordering=['pub_date']
+
+    def __str__(self):
+        return self.message
+
 class Chat(models.Model):
     DIALOG = 'D'
     CHAT = 'C'
@@ -19,20 +31,8 @@ class Chat(models.Model):
         default=DIALOG
     )
     members = models.ManyToManyField(Viber, verbose_name="Member")
- 
+    chat_id = models.ManyToManyField(Message, verbose_name="messages")
     def get_absolute_url(self):
         return 'users:messages', (), {'chat_id': self.pk }
  
  
-class Message(models.Model):
-    author = models.ForeignKey(Viber, verbose_name='viber', on_delete=models.CASCADE)
-    message = models.TextField(max_length=280)
-    pub_date = models.DateTimeField(default=timezone.now)
-    is_read = models.BooleanField(default=False)
-
-    class Meta:
-        ordering=['pub_date']
-
-    def __str__(self):
-        return self.message
-
--- vibetube/urls.py
+++ vibetube/urls.py
@@ -23,16 +23,18 @@ from video.urls import urlpatterns as video_urls
 from vibe_user.urls import urlpatterns as user_urls
 from vibe_auth.urls import urlpatterns as auth_urls
 from vibe_api.urls import urlpatterns as api_urls
+from instant.urls import urlpatterns as msg_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
-
+urlpatterns += msg_urls
 urlpatterns += main_urls
 urlpatterns += video_urls
 urlpatterns += user_urls
 urlpatterns += auth_urls
 urlpatterns += api_urls
+
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,
