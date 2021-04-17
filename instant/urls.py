78a6b8a316371932972833dbbb02f8081bd21ed2
jennifer <ariesgal2017@gmail.com>
2021-04-16 20:16:14 -0700

    got messages SUPER closegit add .git add .git add .!

--- instant/urls.py
+++ instant/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import MessagesView
 
 urlpatterns = [
-    path('messages/<str:recipient>/', MessagesView.as_view(), name='msg')
+    path('messages/new/', MessagesView.new_message, name='new'),
+    path('messages/<int:recipient>/', MessagesView.as_view(), name='msg')
 ]
 
--- instant/views.py
+++ instant/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, reverse
+from django.shortcuts import render, redirect, reverse, HttpResponseRedirect
 from django.views import View
 from .forms import MessageForm
 from .models import Chat, Message
@@ -9,11 +9,10 @@ class MessagesView(View):
     # users = {}
     def get(self, request, recipient):
         # try:
-        messages = Message.objects.all()
+        messages = Message.objects.filter(recipient=request.user.id)
+        print(messages)
         #     chat.message_set.filter(is_read=False).exclude(
         #         author=request.user).update(is_read=True)
-        #     # else:
-        #     #     chat = None
         # except Chat.DoesNotExist:
         #     chat = None
 
@@ -27,12 +26,32 @@ class MessagesView(View):
             }
         )
 
-    def post(self, request, chat_id):
+    def post(self, request, recipient):
         form = MessageForm(data=request.POST)
         if form.is_valid():
             message = form.save(commit=False)
-            message.chat_id = chat_id
+            message.recipient = recipient
             message.author = request.user
             message.save()
-        return redirect(reverse({'message':message}, kwargs={'chat_id': chat_id}))
+        return redirect(reverse({'message':message}, kwargs={'recipient': recipient}))
 
+    def new_message(request):
+        context = {}
+        if request.method == "POST":
+            form = MessageForm(request.POST)
+            if form.is_valid():
+                data = form.cleaned_data
+                new_msg = Message.objects.create(
+                    message=data['message'],
+                    recipient=data['recipient'],
+                )
+                return redirect('/messages/new/')
+        form=MessageForm()   
+        context.update({'message': "Submitted Successfully!!!!! YAY!"})
+        form = MessageForm()
+        context.update({'form': form})
+        return render(
+            request,
+            "messaging/new_message.html", 
+            context
+        )
