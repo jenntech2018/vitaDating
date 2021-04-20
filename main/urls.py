606d384ad9f03d270a77d8dcc0cadc981fcfd129
Jalon Dodson <jalon.dodson@yahoo.com>
2021-04-20 09:25:14 -0500

    fixed url errors

--- main/urls.py
+++ main/urls.py
@@ -1,11 +1,9 @@
 from django.urls import path
 from main.views import MainView
 from vibe_auth.views import login_page, register_page
-from instant.views import MessagesView
 from django.views.static import serve
 
 urlpatterns = [
-    path('message/', MessagesView.as_view(), name='message'),
     path('register/', register_page, name='register'),
     path('login/', login_page, name='login'),
     path("", MainView.as_view(), name="main"),
