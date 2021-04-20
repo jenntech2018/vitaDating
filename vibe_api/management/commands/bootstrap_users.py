51e03059b9c207e6d0220451bebdef49b80d8ff6
Jalon Dodson <jalon.dodson@yahoo.com>
2021-04-20 04:14:16 -0500

    added a bootstrapper for celebs, fixed endpoints

---
+++ vibe_api/management/commands/bootstrap_users.py
@@ -0,0 +1,36 @@
+
+from django.core.management.base import BaseCommand, CommandError
+from vibe_user.models import Viber
+
+class Command(BaseCommand):
+    help = "Bootstraps users usable via manage.py"
+
+    def handle(self, *args, **kwargs):
+        celebrities = [
+            {
+                "display_name":"Justin Bieber",
+                "username":"justinbieber",
+                "email":"justinbieber@thebiebz.com",
+                "bio":"hey go listen to my album it's pretty rad",
+                "website":"www.JustinBieber.lnk.to/Justice",
+            },
+            {
+                "display_name":"Queen Latifah",
+                "username":"thequeen",
+                "email":"queenlatifah@thequeen.com",
+                "bio":"the queen up in this party!!",
+                "website":"queenlatifah.com",
+            }
+        ]
+        for celeb in celebrities:
+            Viber.objects.create(
+                display_name=celeb["display_name"],
+                username=celeb["username"],
+                email=celeb["email"],
+                bio=celeb["bio"],
+                website=celeb["website"],
+                verified=True,
+                password="verysecurepassword123123"
+            )
+
+        self.stdout.write("Added celebs")
